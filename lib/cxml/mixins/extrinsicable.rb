2725f4e7d53abe6f41062758bdc271cd91740d27
rhee <gkrhee@gmail.com>
2025-04-14 15:07:04 -0400

    Add get_extrinsic for extrinsics

--- lib/cxml/mixins/extrinsicable.rb
+++ lib/cxml/mixins/extrinsicable.rb
@@ -21,14 +21,24 @@ module Extrinsicable
 
   # Finds matching extrinisic name and sets new value, will make new extrinsic based on name/value otherwise
   def set_extrinsic(name, value)
+    extrinsic = get_extrinsic name
+    if extrinsic
+      extrinsic.content = value
+      return
+    end
+
+    new_extrinsic = CXML::Extrinsic.new name: name, content: value
+    @extrinsics.push(new_extrinsic)
+  end
+
+  # Finds matching extrinsic
+  def get_extrinsic(name)
     @extrinsics ||= []
     @extrinsics.each do |ex|
       if ex.name == name
-        ex.content = value
-        return
+        return ex
       end
     end
-    new_extrinsic = CXML::Extrinsic.new name: name, content: value
-    @extrinsics.push(new_extrinsic)
+    nil
   end
 end
